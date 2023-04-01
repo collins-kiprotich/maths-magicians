ba2d9d35799e8b6734d489db522527f367b9a7f8
collins kiprotich <collinskiprotich@gmail.com>
2023-04-01 12:09:21 +0300

    Add logic to execute on click events on the calcultor UI

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import Buttons from './Buttons';
+import calculate from '../logic/calculate';
 
-function Calculator() {
-  return (
-    <div className="MainBody">
-      <div className="result">0</div>
-      <Buttons />
-    </div>
-  );
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  clickHandle = (e) => {
+    const clickedBtn = e.target.innerText;
+    const Result = calculate(this.state, clickedBtn);
+    this.setState(Result);
+  }
+
+  render() {
+    const { total, operation, next } = this.state;
+    return (
+      <div className="MainBody">
+        <div className="result">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="num-symb">
+          <button className="buttons" type="button" onClick={this.clickHandle}>AC</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>+/-</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>%</button>
+          <button className="buttons operator" type="button" onClick={this.clickHandle}>÷</button>
+        </div>
+        <div className="num-symb">
+          <button className="buttons" type="button" onClick={this.clickHandle}>7</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>8</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>9</button>
+          <button className="buttons operator" type="button" onClick={this.clickHandle}>x</button>
+        </div>
+        <div className="num-symb">
+          <button className="buttons" type="button" onClick={this.clickHandle}>4</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>5</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>6</button>
+          <button className="buttons operator" type="button" onClick={this.clickHandle}>-</button>
+        </div>
+        <div className="num-symb">
+          <button className="buttons" type="button" onClick={this.clickHandle}>1</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>2</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>3</button>
+          <button className="buttons operator" type="button" onClick={this.clickHandle}>+</button>
+        </div>
+        <div className="num-symb">
+          <button className="buttons" id="bigbtn" type="button" onClick={this.clickHandle}>0</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>.</button>
+          <button className="buttons operator" type="button" onClick={this.clickHandle}>=</button>
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default Calculator;
